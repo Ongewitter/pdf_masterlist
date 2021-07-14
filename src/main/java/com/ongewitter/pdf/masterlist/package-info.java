9b830ba2872ca2cebab4ad8aa7cdc4d606f3a48c
Cedric <cedric@Cedrics-MBP.lan>
2021-07-14 14:50:11 +0200

    Better javadoc

--- src/main/java/com/ongewitter/pdf/masterlist/package-info.java
+++ src/main/java/com/ongewitter/pdf/masterlist/package-info.java
@@ -1,4 +1,13 @@
 /**
- * Info about this package doing something for package-info.java file.
+ * This module is about reading a PDF and exporting a sort of list
+ * <p>
+ * This module reads in an entire PDF
+ * Based on keywords, it then generates a list of hits
+ * It then prints out a PDF of said list in a user-determined order
+ * </p>
+ *
+ * @since 0.1
+ * @author Ongewitter
+ * @version 0.1
  */
 package com.ongewitter.pdf.masterlist;
