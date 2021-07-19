0c649ae9397b8641ecb2546c780b889586af301a
Cedric <cedric@Cedrics-MBP.lan>
2021-07-19 16:10:29 +0200

    Successfully ran program, noticed the PDF is jank. Sadface

--- src/main/java/com/ongewitter/pdf/masterlist/App.java
+++ src/main/java/com/ongewitter/pdf/masterlist/App.java
@@ -1,8 +1,12 @@
 package com.ongewitter.pdf.masterlist;
 
 import java.util.*;
+import java.util.regex.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -36,9 +40,10 @@ public final class App {
     // stripper.setSortByPosition(true);
 
     List<String> paragraphs = new ArrayList<String>();
+    List<String> orphans = new ArrayList<String>();
     // for (int page = 1; page <= document.getNumberOfPages(); ++page) {
     // for (int page = 172; page <= 202; ++page) {
-    for (int page = 175; page <= 175; ++page) {
+    for (int page = 174; page <= 202; ++page) {
         // Set the page interval to extract. If you don't, then all pages would be extracted.
         stripper.setStartPage(page);
         stripper.setEndPage(page);
@@ -50,16 +55,26 @@ public final class App {
 
         String text = stripper.getText(document);
 
-        matchText(text, paragraphs);
+        matchText(text, paragraphs, orphans);
         // do some nice output with a header
         String pageStr = String.format("page %d:", page);
         System.out.println(pageStr);
+        System.out.println("Name\t|\tRoll\t|\tImpact");
         for (int i = 0; i < pageStr.length(); ++i) {
             System.out.print("-");
         }
         System.out.println();
-        System.out.println(text.trim());
-        System.out.println();
+        FileWriter writer = createWriter();
+        try {
+          for (String paragraph : paragraphs) {
+            printParagraph(paragraph, writer);
+          }
+          writer.close();
+        } catch (IOException e) {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+          writer.close();
+        }
 
         // If the extracted text is empty or gibberish, please try extracting text
         // with Adobe Reader first before asking for help. Also read the FAQ
@@ -82,125 +97,13 @@ public final class App {
    * If any are missing add to orphans[]? Most likely to work, emptying out as matches are made :thinking_face:
    */
 
-  // tifying a substance requires time, and in some cases, 
-  // you may need specialized equipment. 
-  // Time: Instantaneous (primary action) to detect poi-
-  // sons; Present Tense for most other uses (at least 10 
-  // minutes spent examining the substance) 
-  // Roll: Alchemy + Perception
-  // Capability: Depending on the substance examined, 
-  // you might need a relevant Lore, Science, or Tech 
-  // Lore.
-  // Resistance: Depends on the rarity of the substance. 
-  // Examples include a common ruby or a well-known 
-  // selchakah in a drink (Hard); or a rare Vril-Ya ant-
-  // lered-wasp venom or an Eridolian pearl (Severe).
-  // Impact: You correctly identify the substance (or lack 
-  // thereof if the drink isn’t spiked).
-  // REFINE 
-  // Action
-  // You may refine a substance, polishing it so that it better 
-  // reflects the invisible light of the Empyrean. This sub-
-  // stance can be material… or your own soul. Refining 
-  // the soul is only transitory (the dross of the world soon 
-  // begrimes it again), but as a meditative form of prayer, it 
-  // can abolish negative mental and social states. 
-  // Time: Narrated. It takes at least eight hours of lab 
-  // work before a roll can be made. Only one attempt 
-  // per day can be made on the same substance.
-  // Roll: Alchemy + Faith
-  // Capability: Depending on the substance to be re-
-  // fined, you might need a relevant Lore, Science, or 
-  // Tech Lore. If this attempt involves your soul, you 
-  // need Religion Lore. 
-  // Resistance: Depends on the raw state of the sub-
-  // stance. Already worked materials — alloys, woven 
-  // textiles — are harder to refine than raw material. 
-  // If the substance is your soul, the Resistance is your 
-  // Will. 
-  // Impact: The substance becomes more spiritually 
-  // pure than before. It is ideal for use as a theurgic 
-  // implement — a phylactery or an object of a theur-
-  // gic Sanctification or Consecration ritual (making 
-  // it into a theurgic vestment). When using a refined 
-  // item as the object of such an imbuement rite, your 
-  // roll is favorable. 
-  // If your opus was aimed at your own soul, you can 
-  // relieve (cancel) one mental or social state that you 
-  // are currently suffering. 
-  // Animalia
-  // Base rating: 3
-  // Animalia represents your understanding of and 
-  // empathy with animals, including non-sentient alien 
-  // creatures. With this skill, you can train animals, and 
-  // IDLE
-  // Action 
-  // See the Restorative Practices sidebar. Spending time just 
-  // reading can help ease anxiety and stir the imagination. 
-  // RESEARCH
-  // Action 
-  // Knowledge is a dangerous weapon. To get that weap-
-  // on, you need to know how to arm yourself. Whether 
-  // digging through a Church library or searching the 
-  // Charioteer guildhall, you can use this maneuver to 
-  // find the knowledge that you seek. 
-  // Time: Narrated. A disorganized archive might re-
-  // quire a full day to comb through, while a well-for-
-  // matted data record might need only half an hour. 
-  // Roll: Academia + Wits 
-  // Capability: Read (the language your topic is written 
-  // in), Think Machine (if it’s in a data archive)
-  // Resistance: Depends on obscurity: Hard for com-
-  // mon knowledge, Demanding for obscure topics, 
-  // and Tough or higher for secret information.
-  // Impact: You gain the knowledge you seek. Halve the 
-  // time required per 2 VP spent. 
-  // Alchemy 
-  // RESTRICTED: Priest, Engineers guild, Banker, Enthu-
-  // siast, Occultist, Spy, Tech Redeemer, Theurgist
-  // Base rating: 0
-  // Alchemy is the art and science of matter trans-
-  // mutation. Those who transmute matter through al-
-  // chemic means also transmute the soul into more re-
-  // fined forms… or so alchemists believe. 
-  // Alchemy integrates aspects of chemistry, philoso-
-  // phy, and physics. It allows you to concoct various po-
-  // tions and substances and to refine implements for use 
-  // with theurgic rituals. As odd as it may seem, alchemy 
-  // actually integrates certain sciences from the Second 
-  // Republic era, although they’re not always fully under-
-  // stood by 51st-century alchemists.
-  // Most uses of this skill require access to an alchemical 
-  // lab: a room stocked with supplies where you can per-
-  // form alchemical works uninterrupted. This could be a 
-  // small backroom in a hovel, a well-lit high-tech guild lab, 
-  // or even a small stateroom or cargo hold on a starship.
-  // Typical capabilities: Applied Science, Religion 
-  // Lore, any relevant Tech Lore for the item or sub-
-  // stance being worked
-  // IDENTIFY SUBSTANCE
-  // Action 
-  // You can identify just about any substance and gauge 
-  // its purity. You can also tell if a glass of wine is drugged 
-  // or poisoned or if a gemstone is real or synthetic. Iden-
-
-
-
-  private static void matchText(final String text, final List<String> paragraphs) {
+  private static void matchText(final String text, final List<String> paragraphs, List<String> orphans) {
     String[] hitOrder = { "Action", "Time:", "Roll:", "Capability:", "Resistance:", "Impact:" };
     String[] hits = text.split("\\n(?=.*\\nAction)"); // Split on Action and previous line
-    List<String> orphans = new ArrayList<String>();
     // Check for orphans and isolate them
-    System.out.println("----------^hits^------------");
-    System.out.println(hits);
-    System.out.println("----------^Gathering hits^------------");
     for(String hit : hits) {
-      System.out.println("----------^singular hit^------------");
-      System.out.println(hit);
       boolean isOrphan = false;
       for (String order : hitOrder) {
-        System.out.println("----------^order^------------");
-        System.out.println(order);
         if (!hit.contains(order)) {
           isOrphan = true;
           orphans.add(hit);
@@ -212,40 +115,92 @@ public final class App {
         paragraphs.add(hit);
       }
     }
-    System.out.println("----------^orphans^------------");
-    System.out.println(orphans);
     // Merge orphans through hitOrder I guess?
-    System.out.println("-----------iterating-----------");
+    List<String> foundOrphans = new ArrayList<String>();
     if (orphans.size() > 0) {
       String merged = "";
       for (String orphan : orphans) {
+        // Filter out orphans that direct you to "See something else"
+        System.out.println("--------------orphan--------------");
+        System.out.println(orphan);
+        System.out.println(orphan.matches("See the.+sidebar"));
+        if (orphan.contains("See the")) {
+          paragraphs.add(orphan);
+          foundOrphans.add(orphan);
+          continue;
+        }
         for (String order : hitOrder) {
-          System.out.println(order);
-          System.out.println(merged);
-          System.out.println(orphan);
-          System.out.println("----------------------");
           if (merged.length() > 0 && merged.contains(order)) { continue; }
           if (orphan.contains(order)) {
-            merged.concat(orphan);
-            orphans.remove(orphan);
+            merged = merged.concat(orphan);
+            foundOrphans.add(orphan);
             break;
           }
         }
+        if (containsAllWords(merged, hitOrder)) {
+          paragraphs.add(merged);
+          merged = "";
+        }
       }
     }
-    
+    // Remove the found orphans
+    orphans.removeAll(foundOrphans);
+    System.out.println("--------------paragraphs--------------");
+    for (String p : paragraphs) {
+      System.out.println(p);
+    }
   }
 
-    // /**
-    //  * Read some CLI input and print some string.
-    //  * @param args The arguments of the program.
-    //  */
-    // public static void main(String[] args) {
-    //   Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-    //   System.out.println("Enter username");
-    //
-    //   String userName = scanner.nextLine();  // Read user input
-    //   scanner.close();
-    //   System.out.println("Username is: " + userName);  // Output user input
-    // }
+  public static boolean containsAllWords(String word, String ...keywords) {
+    for (String k : keywords) {
+      if (!word.contains(k)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Print out the paragraph in the format "Name | Roll | Impact"
+  private static void printParagraph(final String paragraph, final FileWriter writer) throws IOException {
+    Pattern titlePattern = Pattern.compile("(.+)(?=\\nAction)");
+    Matcher m = titlePattern.matcher(paragraph);
+    String line;
+    String title = "SKILL_NAME";
+    if (m.find()) {
+      title = m.group(1);
+    }
+
+    if (paragraph.contains("See the")) {
+      String subParagraph = paragraph.substring(paragraph.indexOf("Action") + 6);
+      line = title + "\t|\t" + subParagraph;
+      writer.write(line);
+      System.out.println(line);
+    } else {
+      Pattern rollPattern = Pattern.compile("Roll:(.+\n)");
+      m = rollPattern.matcher(paragraph);
+      String roll = "SKILL_ROLL";
+      if (m.find()) {
+        roll = m.group(1);
+      }
+  
+      Pattern impactPattern = Pattern.compile("Impact:(.+\n.+\n)");
+      m = impactPattern.matcher(paragraph);
+      String impact = "SKILL_IMPACT";
+      if (m.find()) {
+        impact = m.group(1);
+      }
+  
+      line = title + "\t|\t" + roll + "\t|\t" + impact;
+      writer.write(line);
+      System.out.println(title + "\t|\t" + roll + "\t|\t" + impact);
+    }
+    writer.write(System.getProperty( "line.separator" ));
+    System.out.println();
+  }
+  
+  private static FileWriter createWriter() throws IOException {
+    new File("Maneuvers.txt");
+    FileWriter myWriter = new FileWriter("Maneuvers.txt");
+    return myWriter;
+  }
 }
